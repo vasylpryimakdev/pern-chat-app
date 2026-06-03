13deac962cb8283c2fe993109550e0aff7ee95ea
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-06-03 14:25:35 +0300

    feat: add JWT auth middleware and protect routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,11 +2,13 @@ import express from "express";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
 
+app.use(cookieParser());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,11 +1,10 @@
 import express from "express";
 import { login, logout, signup } from "../controllers/auth.controller.js";
+
 const router = express.Router();
 
-router.get("/login", login);
-
-router.get("/logout", logout);
-
-router.get("/signup", signup);
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
 
 export default router;
