e2f9f5ee437e4f46ee2fae73b60408b8518bc8c5
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-06-03 14:58:24 +0300

    refactor(frontend): switch to Tailwind + Router, remove Vite template

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  // <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </StrictMode>,
-)
+  </BrowserRouter>,
+  // </React.StrictMode>
+);
