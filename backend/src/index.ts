8071e182121ac916a8f43edcd378dc172739384c
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-06-03 15:39:23 +0300

    chore: backend port config + add zustand store and global types

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,8 @@ import cookieParser from "cookie-parser";
 
 dotenv.config();
 
+const PORT = process.env.PORT || 5001;
+
 const app = express();
 
 app.use(cookieParser());
@@ -13,6 +15,6 @@ app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+app.listen(PORT, () => {
+  console.log("Server is running on port " + PORT);
 });
