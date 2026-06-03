7aaa36f551e2c74af0eae1098113a082995684cd
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-06-03 15:05:54 +0300

    feat(frontend): add chat UI pages, components, and dummy data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+
+import { Toaster } from "react-hot-toast";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+
 function App() {
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-      Hello World!
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <Toaster />
     </div>
   );
 }
