9f5644de692f1a54dd845eb6fa80fdb9e0c617d8
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-06-03 14:12:30 +0300

    feat: implement user signup with Prisma and password hashing

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,8 +1,59 @@
 import { Request, Response } from "express";
+import prisma from "../db/prisma.js";
+import bcryptjs from "bcryptjs";
 
 export const signup = async (req: Request, res: Response) => {
-  res.send("Logged in successfully");
+  try {
+    const { fullName, username, password, confirmPassword, gender } = req.body;
+
+    if (!fullName || !username || !password || !confirmPassword || !gender) {
+      return res.status(400).json({ error: "Please fill in all fields" });
+    }
+
+    if (password !== confirmPassword) {
+      return res.status(400).json({ error: "Passwords don't match" });
+    }
+
+    const user = await prisma.user.findUnique({ where: { username } });
+
+    if (user) {
+      return res.status(400).json({ error: "Username already exists" });
+    }
+
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
+
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+    const newUser = await prisma.user.create({
+      data: {
+        fullName,
+        username,
+        password: hashedPassword,
+        gender,
+        profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+      },
+    });
+
+    if (newUser) {
+      // generate token in a sec
+
+      res.status(201).json({
+        id: newUser.id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(400).json({ error: "Invalid user data" });
+    }
+  } catch (error: any) {
+    console.log("Error in signup controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
+
 export const login = async (req: Request, res: Response) => {
   res.send("Logged out successfully");
 };
