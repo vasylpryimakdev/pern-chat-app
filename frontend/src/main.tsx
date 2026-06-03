8e16a50cc89e030dc169bc4196b9136a171623eb
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-06-03 16:15:20 +0300

    Add Socket.IO client with online users indicator

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,13 +3,16 @@ import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { AuthContextProvider } from "./context/AuthContext.tsx";
+import SocketContextProvider from "./context/SocketContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  // <React.StrictMode>
-  <BrowserRouter>
-    <AuthContextProvider>
-      <App />
-    </AuthContextProvider>
-  </BrowserRouter>,
-  // </React.StrictMode>
+	// <React.StrictMode>
+	<BrowserRouter>
+		<AuthContextProvider>
+			<SocketContextProvider>
+				<App />
+			</SocketContextProvider>
+		</AuthContextProvider>
+	</BrowserRouter>
+	// </React.StrictMode>
 );
