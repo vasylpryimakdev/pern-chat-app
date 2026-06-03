08fc84df16a4c9a91fc6174b7cc299006f65c501
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-06-03 16:09:40 +0300

    Add Socket.IO server and integrate with Express

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,20 +1,21 @@
 import express from "express";
-import authRoutes from "./routes/auth.route.js";
-import messageRoutes from "./routes/message.route.js";
-import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+
+import dotenv from "dotenv";
+import { app, server } from "./socket/socket.js";
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
-
-const app = express();
 
 app.use(cookieParser());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-app.listen(PORT, () => {
-  console.log("Server is running on port " + PORT);
+
+server.listen(PORT, () => {
+	console.log("Server is running on port " + PORT);
 });
